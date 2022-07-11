94233f14d55e604388e0e2663279a2d5f8e2694c
yaojixing <yjx@163.com>
2022-07-11 22:02:42 +0800

    first commit

---
+++ UseGit/src/com/atguigu/git.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author YaoJiXing
+ * created 2022/7/11 22:00
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

a53dc46bcda2fe0de13d3e5404944084d2756c44
yaojixing <yjx@163.com>
2022-07-12 10:03:12 +0800

    javaFrame commit2

--- UseGit/src/com/atguigu/GitFirst.java
+++ UseGit/src/com/atguigu/GitFirst.java
@@ -4,7 +4,7 @@ package com.atguigu;
  * @author YaoJiXing
  * created 2022/7/11 22:00
  */
-public class git {
+public class GitFirst {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world two");
---
+++ UseGit/src/com/atguigu/GitTwo.java
@@ -0,0 +1,8 @@
+package com.atguigu;
+
+/**
+ * @author YaoJiXing
+ * created 2022/7/12 10:02
+ */
+public class GitTwo {
+}
